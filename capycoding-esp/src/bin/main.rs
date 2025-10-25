8604687562da45de756c8a09144d26329ec4fd9d
Surendra Jammishetti <suri312006@gmail.com>
2025-10-25 03:09:44 -0700

    clean terminal init

--- capycoding-esp/src/bin/main.rs
+++ capycoding-esp/src/bin/main.rs
@@ -6,36 +6,25 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use alloc::{boxed::Box, fmt::format};
 use bt_hci::controller::ExternalController;
-use display_interface_spi::SPIInterface;
+use capycoding_esp::alloc::fmt::format;
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
-use embedded_hal_bus::spi::ExclusiveDevice;
 use esp_backtrace as _;
 use esp_hal::{
     clock::CpuClock,
-    delay::Delay,
-    gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull},
     spi::{self, master::Spi},
     time::Rate,
     timer::timg::TimerGroup,
 };
 use esp_radio::ble::controller::BleConnector;
-use log::{error, info};
-use mousefood::{EmbeddedBackend, EmbeddedBackendConfig, fonts};
+use log::info;
 use ratatui::{
-    Frame, Terminal,
+    Frame,
     style::{Style, Stylize},
     widgets::{Block, Paragraph, Wrap},
 };
 use trouble_host::prelude::*;
-use weact_studio_epd::{
-    WeActStudio290BlackWhiteDriver,
-    graphics::{Display290BlackWhite, DisplayRotation},
-};
-
-extern crate alloc;
+use weact_studio_epd::graphics::Display290BlackWhite;
 
 const CONNECTIONS_MAX: usize = 1;
 const L2CAP_CHANNELS_MAX: usize = 1;
@@ -57,6 +46,7 @@ async fn main(spawner: Spawner) -> ! {
     // COEX needs more RAM - so we've added some more
     esp_alloc::heap_allocator!(size: 200 * 1024);
 
+    // * RTOS init *
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     let sw_interrupt =
         esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
@@ -64,10 +54,13 @@ async fn main(spawner: Spawner) -> ! {
 
     info!("Embassy initialized!");
 
+    // * WIFI *
     let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
     let (mut _wifi_controller, _interfaces) =
         esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
             .expect("Failed to initialize Wi-Fi controller");
+
+    // * BLE *
     // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
     let transport = BleConnector::new(&radio_init, peripherals.BT, Default::default()).unwrap();
     let ble_controller = ExternalController::<_, 20>::new(transport);
@@ -78,68 +71,32 @@ async fn main(spawner: Spawner) -> ! {
     // TODO: Spawn some tasks
     let _ = spawner;
 
-    let mosi_pin = peripherals.GPIO5; // SPI MOSI
-    let sclk_pin = peripherals.GPIO4; // SPI SCLK
-    let cs_pin = peripherals.GPIO3; // CS
-    let dc_pin = peripherals.GPIO2; // DC
-    let rst_pin = peripherals.GPIO1; // RST
-    let busy_pin = peripherals.GPIO0; // BUSY (input)
-
-    // Create pin drivers for bare pins
-    // /*
-    //     CS: OutputPin,
-    //     BUSY: InputPin,
-    //     DC: OutputPin,
-    //     RST: OutputPin,
-    // */
-    let cs = Output::new(cs_pin, Level::High, OutputConfig::default());
-    let busy = Input::new(busy_pin, InputConfig::default().with_pull(Pull::Up));
-    let dc = Output::new(dc_pin, Level::Low, OutputConfig::default());
-    let rst = Output::new(rst_pin, Level::High, OutputConfig::default());
-    let delay = Delay::new();
-
-    let spi_bus = {
-        info!("Intializing SPI Bus...");
-
-        let config = spi::master::Config::default()
-            .with_frequency(Rate::from_khz(100))
-            .with_mode(spi::Mode::_0);
-
-        Spi::new(peripherals.SPI2, config)
-            .inspect_err(|e| {
-                error!("Error while creating SPI: {e}");
-            })
-            .unwrap()
-            .with_sck(sclk_pin)
-            .with_mosi(mosi_pin)
-    };
-
-    info!("Intializing SPI Device...");
-    let spi_device = ExclusiveDevice::new(spi_bus, cs, delay.clone())
-        .inspect_err(|e| error!("Error creating exclusive spi device: {e}"))
-        .unwrap();
-
-    let spi_interface = SPIInterface::new(spi_device, dc);
-
-    // Setup EPD
-    log::info!("Intializing EPD...");
-    let mut driver = WeActStudio290BlackWhiteDriver::new(spi_interface, busy, rst, delay.clone());
     let mut display = Display290BlackWhite::new();
-    display.set_rotation(DisplayRotation::Rotate90);
-    driver.init().unwrap();
 
-    let config = EmbeddedBackendConfig {
-        font_regular: fonts::MONO_10X20,
-        flush_callback: Box::new(move |d| {
-            driver.fast_update(d).unwrap();
-            // driver.full_update(d).unwrap();
-            // driver.fast_refresh().unwrap();
-        }),
-        ..Default::default()
-    };
-    let backend = EmbeddedBackend::new(&mut display, config);
+    let mosi_pin = peripherals.GPIO5;
+    let sclk_pin = peripherals.GPIO4;
 
-    let mut terminal = Terminal::new(backend).unwrap();
+    let spi_bus: Spi<'_, esp_hal::Blocking> = Spi::new(
+        peripherals.SPI2,
+        spi::master::Config::default()
+            .with_frequency(Rate::from_khz(100))
+            .with_mode(spi::Mode::_0),
+    )
+    .unwrap()
+    .with_sck(sclk_pin)
+    .with_mosi(mosi_pin);
+
+    // let mut terminal = capycoding_esp::setup_terminal(peripherals, &mut display);
+    let mut terminal = capycoding_esp::setup_weact_term(
+        spi_bus,
+        &mut display,
+        peripherals.GPIO3,
+        peripherals.GPIO2,
+        peripherals.GPIO1,
+        peripherals.GPIO0,
+    );
+
+    info!("Terminal initialized!");
 
     // Run an infinite loop, where widgets will be rendered
     let mut i = 0;
@@ -157,6 +114,9 @@ async fn main(spawner: Spawner) -> ! {
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.1/examples/src/bin
 }
 
+#[embassy_executor::task]
+async fn render() {}
+
 fn draw(frame: &mut Frame, i: &mut i32) {
     let text = format(format_args!("Ratatui on embedded devices! {i}"));
     let paragraph = Paragraph::new(text.dark_gray()).wrap(Wrap { trim: true });
@@ -165,3 +125,47 @@ fn draw(frame: &mut Frame, i: &mut i32) {
         .title("Mousefood");
     frame.render_widget(paragraph.block(bordered_block), frame.area());
 }
+
+// fn setup_terminal(peripherals: &mut Peripherals) -> Terminal<EmbeddedBackend<'_>> {
+//     let mosi_pin = peripherals.GPIO5;
+//     let sclk_pin = peripherals.GPIO4;
+//     let cs_pin = peripherals.GPIO3;
+//     let dc_pin = peripherals.GPIO2;
+//     let rst_pin = peripherals.GPIO1;
+//     let busy_pin = peripherals.GPIO0;
+
+//     let cs = Output::new(cs_pin, Level::High, OutputConfig::default());
+//     let busy = Input::new(busy_pin, InputConfig::default().with_pull(Pull::Up));
+//     let dc = Output::new(dc_pin, Level::Low, OutputConfig::default());
+//     let rst = Output::new(rst_pin, Level::High, OutputConfig::default());
+//     let delay = Delay::new();
+
+//     let spi_bus = Spi::new(
+//         peripherals.SPI2,
+//         spi::master::Config::default()
+//             .with_frequency(Rate::from_khz(100))
+//             .with_mode(spi::Mode::_0),
+//     )
+//     .unwrap()
+//     .with_sck(sclk_pin)
+//     .with_mosi(mosi_pin);
+
+//     let spi_device = ExclusiveDevice::new(spi_bus, cs, delay.clone()).unwrap();
+//     let spi_interface = SPIInterface::new(spi_device, dc);
+
+//     let mut driver = WeActStudio290BlackWhiteDriver::new(spi_interface, busy, rst, delay.clone());
+//     let mut display = Display290BlackWhite::new();
+//     display.set_rotation(DisplayRotation::Rotate90);
+//     driver.init().unwrap();
+
+//     let config = EmbeddedBackendConfig {
+//         font_regular: fonts::MONO_10X20,
+//         flush_callback: Box::new(move |d| {
+//             driver.fast_update(d).unwrap();
+//         }),
+//         ..Default::default()
+//     };
+
+//     let backend = EmbeddedBackend::new(&mut display, config);
+//     Terminal::new(backend).unwrap()
+// }
--- capycoding-esp/src/lib.rs
+++ capycoding-esp/src/lib.rs
@@ -1,1 +1,69 @@
 #![no_std]
+
+use alloc::boxed::Box;
+use display_interface_spi::SPIInterface;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use weact_studio_epd::{
+    WeActStudio290BlackWhiteDriver,
+    graphics::{Display, DisplayRotation},
+};
+
+use mousefood::{EmbeddedBackend, EmbeddedBackendConfig, fonts};
+use ratatui::Terminal;
+
+use esp_hal::{
+    Blocking,
+    delay::Delay,
+    gpio::{self, Input, InputConfig, Level, Output, OutputConfig, Pull},
+    spi::master::Spi,
+};
+pub extern crate alloc;
+
+pub type CapyDisplay = Display<128, 296, 4736, weact_studio_epd::Color>;
+
+pub type CapyTerm<'a> = Terminal<
+    EmbeddedBackend<'a, Display<128, 296, 4736, weact_studio_epd::Color>, weact_studio_epd::Color>,
+>;
+
+pub fn setup_weact_term<'a, CS, DC, RST, BUSY>(
+    spi: Spi<'static, Blocking>,
+    display: &'a mut CapyDisplay,
+    cs_pin: CS,
+    dc_pin: DC,
+    rst_pin: RST,
+    busy_pin: BUSY,
+) -> CapyTerm<'a>
+where
+    CS: gpio::OutputPin + 'static,
+    DC: esp_hal::gpio::OutputPin + 'static,
+    RST: esp_hal::gpio::OutputPin + 'static,
+    BUSY: esp_hal::gpio::InputPin + 'static,
+{
+    let cs = Output::new(cs_pin, Level::High, OutputConfig::default());
+    let busy = Input::new(busy_pin, InputConfig::default().with_pull(Pull::Up));
+    let dc = Output::new(dc_pin, Level::Low, OutputConfig::default());
+    let rst = Output::new(rst_pin, Level::High, OutputConfig::default());
+    let delay = Delay::new();
+
+    let spi_device = ExclusiveDevice::new(spi, cs, delay.clone()).unwrap();
+
+    let spi_interface = SPIInterface::new(spi_device, dc);
+
+    let mut driver = WeActStudio290BlackWhiteDriver::new(spi_interface, busy, rst, delay.clone());
+    display.set_rotation(DisplayRotation::Rotate90);
+    driver.init().unwrap();
+
+    let config = EmbeddedBackendConfig {
+        font_regular: fonts::MONO_10X20,
+        flush_callback: Box::new(move |d| {
+            driver.fast_update(d).unwrap();
+        }),
+        ..Default::default()
+    };
+
+    let backend = EmbeddedBackend::new(display, config);
+
+    let term = Terminal::new(backend).unwrap();
+
+    term
+}
