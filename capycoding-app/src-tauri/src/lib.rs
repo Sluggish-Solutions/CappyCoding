01fe57bfafbb157d75de08665b17739cd887bc35
Surendra Jammishetti <suri312006@gmail.com>
2025-10-25 08:01:03 -0700

    taurpc basic setup

--- capycoding-app/src-tauri/src/lib.rs
+++ capycoding-app/src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use crate::types::{Id, Info};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,11 +11,38 @@ fn call_api() {
     println!("I was invoked from JavaScript!");
 }
 
+#[taurpc::procedures(export_to = "../src/types.ts")]
+trait Api {
+    async fn hello_world() -> String;
+
+    async fn get_user(id: Id) -> Info;
+}
+
+#[derive(Clone)]
+struct ApiImpl;
+
+#[taurpc::resolvers]
+impl Api for ApiImpl {
+    async fn hello_world(self) -> String {
+        "Penis".to_owned()
+    }
+
+    async fn get_user(self, id: Id) -> Info {
+        Info {
+            id,
+            first_name: "Surendra".to_owned(),
+            last_name: "Jammishetti".to_owned(),
+        }
+    }
+}
+
+mod types;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, call_api])
+        .invoke_handler(taurpc::create_ipc_handler(ApiImpl.into_handler()))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
